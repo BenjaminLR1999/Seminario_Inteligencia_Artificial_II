40c60e19260dbcd400df9dcd21ddb49b3523869e
Benjamin <75349048+BenjaminLR1999@users.noreply.github.com>
2023-09-15 14:39:32 -0600

    Update Practica 1. Ejercicio 1.py

--- Practica 1. Ejercicio 1.py
+++ Practica 1. Ejercicio 1.py
@@ -1,43 +1,67 @@
 #Practica 1. Ejercicio 1
 
-#LIBRERIAS
+#DATOS ENTRADA (PRUEBA)
+#X1 = 1.0145
+#X2 = 1.045
+#RESULTADO = -1
+
+# LIBRERIAS
 import random
 
-#VARIABLES
-X1  = 0     #ENTRADA
-X2  = 0     #ENTRADA
-B   = 1     #BIAS
-WX1 = 0     #PESO
-WX2 = 0     #PESO
-WXB = 0     #PESO
-Y   = 0     #SALIDA
-Sumatoria = 0
+# VARIABLES
+Factor_Aprendizaje = 0.4;     # FACTOR DE APRENDIZAJE
+Salida_Deseada     = -1;      # SALIDA DESEADA
+X1                 = 0;       # ENTRADA 1
+X2                 = 0;       # ENTRADA 2
+B                  = 1;       # ENTRADA BIAS
+W1                 = 0;       # PESO 1
+W2                 = 0;       # PESO 2
+WB                 = 0;       # PESO BIAS
+Y                  = 0;       # SALIDA
+Error              = 0;       # ERROR
 
-print("Practica 1. Ejercicio 1")
-print("-- Perceptron Simple --", "\n")
+print("Practica 1. Ejercicio 1");
+print("-- Perceptron Simple --", "\n");
 
-X1 = float(input("Ingresa X1: "))
-X2 = float(input("Ingresa X2: "))
+# ENTRADAS
+X1 = float(input("Ingresa X1: "));
+x2 = float(input("Ingresa X2: "));
 
-#PESOS ENTRE -1 Y 1
-WX1 = random.uniform(-1, 1)
-WX2 = random.uniform(-1, 1)
-WXB = random.uniform(-1, 1)
+# PESOS ALEATORIOS (-1, 1)
+W1 = random.uniform(-1, 1);
+W2 = random.uniform(-1, 1);
+WB = random.uniform(-1, 1);
 
-#BIAS
-B = B * WXB
+# BIAS
+B = B * WB;
 
-#SUMATORIA
-Sumatoria = (X1 * WX1) + (X2 * WX2) + B
+# CICLO WHILE 
+while (Error != 0):
 
-#FUNCION DE ACTIVACION
-if (Sumatoria >= 0):
+    # SUMATORIA 
+    Y = (X1 * W1) + (X2 * W2) + B;
 
-    Y = 1
+    # FUNCION DE ACTIVACION
+    # Y >= 0 --> Y =  1
+    # Y <  0 --> Y = -1
+    if (Y >= 0):
+
+        Y = 1
+
+    elif (Y < 0):
     
-else:
+        Y = -1
 
-    Y = 0
+    # ERROR 
+    Error = Salida_Deseada - Y;
 
-print("Resultado Sumatoria:  ", Sumatoria)
-print("Resultado Salida (Y): ", Y)
+    if (Error != 0):
+        # RECALCULAR PESOS
+        WB = WB + (Factor_Aprendizaje * Error * B);
+        W1 = W1 + (Factor_Aprendizaje * Error * B);
+        W2 = W2 + (Factor_Aprendizaje * Error * B);  
+
+# RESULTADO: PESOS IDEALES
+print("Peso W1: ", W1);
+print("Peso W2: ", W2);
+print("Peso WB: ", WB);
